32807a4a1f23dcf43f25a6bb7bb4e22105f4fc09
annkaniga <iam@annkani.ga>
2018-04-06 04:26:18 +0000

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(8980, () => console.log('listening on port 8980'))
